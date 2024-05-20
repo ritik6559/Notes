16d278584b914506266e0bc8558dff8005d9feff
Ritik6559 <ritikjoshi741@gmail.com>
2024-05-20 14:04:38 +0530

    adding details page

--- lib/pages/notes_details_page.dart
+++ lib/pages/notes_details_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:mynotes/database/notes_database.dart';
+import 'package:mynotes/model/note.dart';
 
 class NoteDetailPage extends StatefulWidget {
   final int noteId;
@@ -9,9 +12,74 @@ class NoteDetailPage extends StatefulWidget {
 }
 
 class _NoteDetailPageState extends State<NoteDetailPage> {
-  
+  late Note note;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    refreshNote();
+  }
+
+  Future refreshNote() async {
+    setState(() {
+      isLoading = true;
+    });
+
+    note = await NotesDatabase.instance.readNote(widget.noteId);
+
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.edit),
+          ),
+          IconButton(
+              onPressed: () async {
+                await NotesDatabase.instance.delete(widget.noteId);
+                // ignore: use_build_context_synchronously
+                Navigator.of(context).pop();
+              },
+              icon: const Icon(Icons.delete))
+        ],
+      ),
+      body: isLoading
+          ? const CircularProgressIndicator()
+          : Padding(
+              padding: const EdgeInsets.all(12),
+              child: ListView(
+                children: [
+                  Text(
+                    note.title,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(note.createdTime),
+                    style: const TextStyle(color: Colors.white38),
+                  ),
+                  Text(
+                    note.description,
+                    style: const TextStyle(color: Colors.white70, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+    );
   }
 }
